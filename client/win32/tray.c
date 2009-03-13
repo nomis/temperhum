2352d9095b43761f0aa184e994fce637d998dde7
Simon Arlott <simon.arlott.org.uk>
2009-03-13 19:28:54 +0000

    add digit images

--- client/win32/tray.c
+++ client/win32/tray.c
@@ -18,6 +18,21 @@
 #include "debug.h"
 #include "tray.h"
 
+#include "digit_dash.xbm"
+#include "digit_dot.xbm"
+#include "digit_zero.xbm"
+#include "digit_one.xbm"
+#include "digit_two.xbm"
+#include "digit_three.xbm"
+#include "digit_four.xbm"
+#include "digit_five.xbm"
+#include "digit_six.xbm"
+#include "digit_seven.xbm"
+#include "digit_eight.xbm"
+#include "digit_nine.xbm"
+
+char icon_buf[16*16];
+
 void update_tray(struct tray_status *status) {
 	odprintf("tray: conn=%d, temperature_celsius=%f, relative_humidity=%f, dew_point=%f",
 		status->conn, status->temperature_celsius, status->relative_humidity, status->dew_point);
