5f3ff924961e09bb74f54a54aca90ba03221d5e7
Simon Arlott <simon.arlott.org.uk>
2010-08-22 21:15:45 +0100

    add config file

--- client/pachube/temppachube.py
+++ client/pachube/temppachube.py
@@ -6,44 +6,18 @@ import socket
 import sys
 import time
 
-sensors = {
-	"front_room": "front-bedroom",
-	"rear_room": "rear-bedroom",
-	"comp": "computers"
-}
-
-def process(feed, c, id, card, mux, data):
-	global muxes, stats
-
-	_strength = []
-	_quality = []
-	_signal = []
-
-	for line in data.split("\n"):
-		match = stats.match(line)
-		if not match:
-			continue
-		match = match.groupdict()
-
-		strength = float(match["signal"]) / 655.36
-		quality = float(match["snr"]) / 655.36
-		signal = float(match["signal"]) / 655.36 * (float(match["snr"]) / 65536.0)
-
-		c.execute("INSERT INTO stats (card, mux, strength, quality) VALUES(%(card)s, %(mux)s, %(strength)s, %(quality)s)", { "card": card, "mux": mux, "strength": strength, "quality": quality })
-
-		_strength.append(strength)
-		_quality.append(quality)
-		_signal.append(signal)
-
-	if len(_strength) > 0:
-		feed.update([eeml.Data("{0}.strength".format(id), "{0:.4f}".format(float(sum(_strength)) / len(_strength)))])
-	if len(_quality) > 0:
-		feed.update([eeml.Data("{0}.quality".format(id), "{0:.4f}".format(float(sum(_quality)) / len(_quality)))])
-	if len(_signal) > 0:
-		feed.update([eeml.Data("{0}.signal".format(id), "{0:.4f}".format(float(sum(_signal)) / len(_signal)))])
-
 with open("key") as f:
 	key = f.read().rstrip()
+
+sensors = {}
+
+with open("config") as f:
+	while True:
+		line = f.readline()
+		if line is None or line == "":
+			break
+		(name, value) = line.rstrip().split(" ")
+		sensors[name] = value
 
 if len(sys.argv) < 2:
 	print("Usage: temppachube.py <id> [host] [port]")
